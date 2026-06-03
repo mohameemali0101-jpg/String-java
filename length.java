1e7cdeca01d281581636cb0e0be42e695f80bc09
mohameemali0101-jpg <mohameemali0101@gmail.com>
2026-06-03 11:28:06 +0530

    Add files via upload

---
+++ length.java
@@ -0,0 +1,13 @@
+package com.tcs.prep;
+
+public class duplicate {
+
+	///public duplicate() {
+		public static void main (String[] args) {
+			String original = ("hello world");
+			int len = original.length();
+			System.out.println("length is "+len);
+		}
+	}
+
+
