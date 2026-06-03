a40eeb75f60a8cab695f3ab1b18c08022bea936d
mohameemali0101-jpg <mohameemali0101@gmail.com>
2026-06-03 11:26:21 +0530

    Add files via upload

---
+++ Duplicate.java
@@ -0,0 +1,26 @@
+package com.tcs.prep;
+
+public class Duplicate {
+
+	public static void main(String[] args) {
+		String str = "banana";
+		for (int i = 0; i < str.length(); i++) {
+			int count = 1;
+			for (int j = i + 1; j < str.length(); j++) {
+				if (str.charAt(i) == str.charAt(j)) {
+					count++;
+				}
+			}
+			boolean alreadyPrinted = false;
+			for (int k = 0; k < i; k++) {
+				if (str.charAt(i) == str.charAt(k)) {
+					alreadyPrinted = true;
+					break;
+				}
+			}
+			if (count >1&&!alreadyPrinted) {
+				System.out.println(str.charAt(i) + "=" + count);
+			}
+		}
+	}
+}
